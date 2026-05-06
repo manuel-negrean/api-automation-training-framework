e4cc6bc6d48d3b0501d3af68b583f75bc38045fd
Cosmin Marius Hojbota <marius.hojbota@codecrafters.ro>
2026-05-06 15:26:29 +0300

    Add improved Pet tests

---
+++ src/main/java/utils/TestDataFactory.java
@@ -0,0 +1,19 @@
+package utils;
+
+import dto.PetDto;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TestDataFactory {
+        public static PetDto createPet(long id, String petName, PetDto.CategoryDto categoryName, PetDto.TagDto petTag, String petStatus) {
+            PetDto petDto = new PetDto();
+            petDto.setId(id);
+            petDto.setName(petName);
+            petDto.setCategory(categoryName);
+            petDto.setTags(Collections.singletonList(petTag));
+            petDto.setStatus(petStatus);
+            petDto.setPhotoUrls(Collections.emptyList());
+            return petDto;
+        }
+}
