c24b5e217f567ba77cf86c6b4c9ed1bfdd623535
Florentinaf1 <florentina.fetco@codecrafters.ro>
2026-05-13 09:19:36 +0300

    Add improved tests

--- src/test/java/tests/PetTestsImproved.java
+++ src/test/java/tests/PetTestsImproved.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import dto.PetDto;
+import org.testng.annotations.AfterClass;
 import utils.TestDataFactory;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +12,10 @@ import static io.restassured.RestAssured.given;
 
 @Slf4j
 public class PetTestsImproved extends BaseTest {
-    
+    private static long petId;
+
     @Test
-    public void  createPet() {
+    public void createPet() {
         long id = System.currentTimeMillis();
         PetDto petDto = TestDataFactory.createPet(id, "Azorel", new PetDto.CategoryDto(1, "CaineMare"), new PetDto.TagDto(1, "Cuminte"), "available");
         Response response = given()
@@ -28,9 +30,62 @@ public class PetTestsImproved extends BaseTest {
                 .extract()
                 .response();
         PetDto createPet = response.as(PetDto.class);
+        petId = createPet.getId();
         log.info("Create pet:" + createPet);
-        
-                
-                
+
+        // tema findPEt by status, update, delete , add,  commit si push
+
+
+    }
+
+    @Test
+    public void findPetByStatus() {
+        Response response = given()
+                .spec(requestSpecification)
+                .queryParam("status", "available")
+                .log().all()
+                .when()
+                .get("/pet/findByStatus")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+
+    }
+
+    @Test
+    public void updatePet() {
+
+        PetDto updatePet = TestDataFactory.createPet(petId, "Grivei", new PetDto.CategoryDto(1, "CaineMare"), new PetDto.TagDto(1, "Cuminte"), "sold");
+        Response updateResponse = given()
+                .spec(requestSpecification)
+                .body(updatePet)
+                .log().all()
+                .when()
+                .put("/pet")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+        PetDto updatedPet = updateResponse.as(PetDto.class);
+        log.info("Updated pet:" + updatedPet);
+    }
+
+    @Test
+    public void deletePet() {
+        Response response = given()
+                .spec(requestSpecification)
+                .log().all()
+                .when()
+                .delete("/pet/" + petId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .response();
+         log.info("Deleted pet with id: " + petId);
     }
 }
+
